7b004abc1318a796622caea4e7a33225f77b83ec
oman1219 <32309792+oman1219@users.noreply.github.com>
2021-06-11 18:24:18 -0400

    fix server

--- server.js
+++ server.js
@@ -211,5 +211,14 @@ app.post('/api/score/:roundNum/:newscores', (req, res) => {
    res.json({ status: 'success'  });
    }
 });
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+
+  // Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
